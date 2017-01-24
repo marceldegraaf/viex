9776f7ebdf6732a4b9124c485ca064484bfd0c4a
Marcel de Graaf <mail@marceldegraaf.net>
2017-01-24 22:17:59 +0100

    initial commit is initial

---
+++ lib/viex.ex
@@ -0,0 +1,18 @@
+defmodule Viex do
+  @moduledoc """
+  Documentation for Viex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Viex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/viex_test.exs
@@ -0,0 +1,8 @@
+defmodule ViexTest do
+  use ExUnit.Case
+  doctest Viex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
